9b9e289bb2d02012d00d4880ef2b93e2ad4bf317
Andrea Mario Lufino <andrea.lufino@me.com>
2013-07-19 15:03:46 +0200

    New documentation comments

--- ALQuickFrame/ALQuickFrame/UIView+ALQuickFrame.h
+++ ALQuickFrame/ALQuickFrame/UIView+ALQuickFrame.h
@@ -6,24 +6,77 @@
 //  Copyright (c) 2013 Andrea Mario Lufino. All rights reserved.
 //
 
+
+// This category is also available as source code
+// on github at this link :
+// https://github.com/andrealufino/ALQuickFrame
+
 #import <UIKit/UIKit.h>
+
+/*!
+ * This category allows to access coordinates and size of any view directly
+ */
 
 @interface UIView (ALQuickFrame)
 
-//X and Y parameters
+/*!
+ Access the x coordinate
+ @return The x coordinate of the view
+ */
 - (CGFloat)x;
+
+/*!
+ Set the new x coordinate
+ @param updatedX The new x to set
+ */
 - (void)setX:(CGFloat)updatedX;
+
+/*!
+ Access the y coordinate
+ @return The y coordinate of the view
+ */
 - (CGFloat)y;
+
+/*!
+ Set the new y coordinate
+ @param updatedY The new y to set
+ */
 - (void)setY:(CGFloat)updatedY;
 
-//Width and Height parameters
+/*!
+ Access the width property
+ @return The width of the view
+ */
 - (CGFloat)width;
+
+/*!
+ Set the new width
+ @param updatedWidth The new width
+ */
 - (void)setWidth:(CGFloat)updatedWidth;
+
+/*!
+ The height of the view
+ @return The height of the view
+ */
 - (CGFloat)height;
+
+/*!
+ Set the new height
+ @param updatedHeight The new height
+ */
 - (void)setHeight:(CGFloat)updatedHeight;
 
-//Size parameter
+/*!
+ The CGSize of the view
+ @return The size (CGSize) of the view
+ */
 - (CGSize)size;
+
+/*!
+ Set the new size
+ @param updatedSize The new size
+ */
 - (void)setSize:(CGSize)updatedSize;
 
 @end
